8302eeda809b1c30f9c6cdaf2732039d0b71b313
Dharmaraj Jadeja <dharmaraj.jadeja911@gmail.com>
2021-01-25 18:50:28 +0530

    v0.2 Added log, reset btn and lapbtn

--- Stop Watch/Scripts/app.js
+++ Stop Watch/Scripts/app.js
@@ -2,25 +2,32 @@ const disp = document.getElementById('disp');
 const startBtn = document.getElementById('start-btn');
 const stopBtn = document.getElementById('stop-btn');
 const lapBtn = document.getElementById('lap-btn');
+const resetBtn = document.getElementById('reset-btn');
 
 
 //let ms = 00;
 let ss = 00;
 let mn = 00;
 let hr = 00;
+let cnt = 0;
+let lapCtn = 0;
+let lapT = 0;
+let lapLabel = document.getElementById('lap');
+let totalTime = document.getElementById('total-time');
+let lapTime = document.getElementById('lap-time');
 //localStorage.setItem('milesecond', ms);
 let strtInterval;
-
-let d = new Date();
+let str = `${hr.toString()}:${mn.toString()}:${ss.toString()}`;
+/* let d = new Date(); */
 
 const setVal = () => {
     ms = localStorage.getItem('milesecond');
     ss = localStorage.getItem('second');
     mn = localStorage.getItem('minute');
-    hr = localStorage.getItem('hour');    
+    hr = localStorage.getItem('hour');
     console.log('Page loaded');
-    let str = `${hr.toString()}:${mn.toString()}:${ss.toString()}`
-    disp.innerHTML = str2                                                           
+    str = `${hr.toString()}:${mn.toString()}:${ss.toString()}`;
+    disp.innerHTML = str;
 }
 
 const startTimmer = () => {
@@ -47,16 +54,66 @@ const startTimmer = () => {
 // Will stop the startInterval function on  the "Start" btn
 const stopTimmer = () => {
     clearInterval(strtInterval);
-    let str = `${hr.toString()}:${mn.toString()}:${ss.toString()}:${ms.toString()}`;
+    str = `${hr.toString()}:${mn.toString()}:${ss.toString()}`;
 }
 
+const resetTimmer = () => {
+    logHistory(ss, mn, hr, lapLabel);
+    ss = 00;
+    mn = 00;
+    hr = 00;
+    lapLabel = 0;
+    stopTimmer();
+    disp.innerHTML = str
+    console.log(str);
+}
+
+// History
+const logHistory = (ss, mn, hr, lapLabel) => {
+    let p = document.getElementById('history');
+    //let pLi = document.getElementById('lap-list');
+
+    str = `${hr.toString()}:${mn.toString()}:${ss.toString()}`;
+    //let li = document.getElementById('history-list');
+    let li = document.createElement('p');
+    li.innerHTML = `${str} lap: ${lapCtn}`;
+    if (cnt < 10) {
+        p.appendChild(li);
+    } else {
+        p.insertBefore(li, p.childNodes[0]);
+        //alert('Only 10 history allowed!!');
+    }
+    cnt++;
+}
+
+//Lapping the time
+const lapping = () => {
+    lapCtn++;
+    let tmp_ss = 00;
+    let tmp_mn = 00;
+    let tmp_hr = 00;
+    let lap = document.getElementById('lap');
+    lap.innerHTML = lapCtn;
+    str = `${hr.toString()}:${mn.toString()}:${ss.toString()}`;
+    // lapT = str;
+    lapTime.innerHTML = str;
+    lapLabel.innerHTML = lapCtn;
+}
 
 // Start the watch
 startBtn.addEventListener('click', () => {
+    ss++;
+    disp.innerHTML = str;
     strtInterval = setInterval(startTimmer, 1000);
 });
 
 // Stopping the interval
 stopBtn.addEventListener('click', stopTimmer);
+
+//Lapping the time
+lapBtn.addEventListener('click', lapping);
+
+// Reset button
+resetBtn.addEventListener('click', resetTimmer);
 
 window.addEventListener('load', setVal)

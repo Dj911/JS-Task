96ac8d3d2caf2f047e87dd2fe748c5c26587bb0e
Dharmaraj Jadeja <dharmaraj.jadeja911@gmail.com>
2021-01-27 10:51:14 +0530

    v0.2 All buttons working, only browser close functionality remaining

--- Stop Watch/Scripts/app.js
+++ Stop Watch/Scripts/app.js
@@ -9,6 +9,9 @@ const resetBtn = document.getElementById('reset-btn');
 let ss = 00;
 let mn = 00;
 let hr = 00;
+let tmp_ss = 0;
+let tmp_mn = 0;
+let tmp_hr = 0;
 let cnt = 0;
 let lapCtn = 0;
 let lapT = 0;
@@ -17,6 +20,8 @@ let totalTime = document.getElementById('total-time');
 let lapTime = document.getElementById('lap-time');
 //localStorage.setItem('milesecond', ms);
 let strtInterval;
+let startBtnCnt = 0;
+let ltime;
 let str = `${hr.toString()}:${mn.toString()}:${ss.toString()}`;
 /* let d = new Date(); */
 
@@ -48,11 +53,13 @@ const startTimmer = () => {
             mn = 0;
         }
     }
-
+    //clearInterval(strtInterval);
 }
 
 // Will stop the startInterval function on  the "Start" btn
 const stopTimmer = () => {
+    //startBtn.hidden = false;
+    startBtnCnt = 0;
     clearInterval(strtInterval);
     str = `${hr.toString()}:${mn.toString()}:${ss.toString()}`;
 }
@@ -62,7 +69,13 @@ const resetTimmer = () => {
     ss = 00;
     mn = 00;
     hr = 00;
-    lapLabel = 0;
+    tmp_ss = 0;
+    tmp_mn = 0;
+    tmp_hr = 0;
+    lapCtn = 0;
+    lapLabel.innerHTML = 0;
+    lapTime.innerText = '';
+    totalTime.innerText = '';
     stopTimmer();
     disp.innerHTML = str
     console.log(str);
@@ -89,22 +102,30 @@ const logHistory = (ss, mn, hr, lapLabel) => {
 //Lapping the time
 const lapping = () => {
     lapCtn++;
-    let tmp_ss = 00;
-    let tmp_mn = 00;
-    let tmp_hr = 00;
     let lap = document.getElementById('lap');
+    ltime = `${hr - tmp_hr}:${mn - tmp_mn}:${ss - tmp_ss}`;
+    tmp_ss = ss;
+    tmp_mn = mn;
+    tmp_hr = hr;
+    console.log(tmp_hr, tmp_mn, tmp_ss);
     lap.innerHTML = lapCtn;
     str = `${hr.toString()}:${mn.toString()}:${ss.toString()}`;
     // lapT = str;
-    lapTime.innerHTML = str;
+    totalTime.innerHTML = str;
+    lapTime.innerHTML = ltime;
     lapLabel.innerHTML = lapCtn;
 }
 
 // Start the watch
 startBtn.addEventListener('click', () => {
-    ss++;
-    disp.innerHTML = str;
-    strtInterval = setInterval(startTimmer, 1000);
+    if (startBtnCnt === 0) {
+        startBtnCnt++;
+        ss++;
+        disp.innerHTML = str;
+        strtInterval = setInterval(startTimmer, 1000);
+        //startBtn.hidden = true;
+        //startBtn.classList.toggle('visibility');
+    }
 });
 
 // Stopping the interval

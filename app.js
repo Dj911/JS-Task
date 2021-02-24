b4e4e02627fd4c0e3261c4aa952f49c6ace31cf1
DJ911 <dharmaraj.jadeja@solulab.co>
2021-02-24 16:00:40 +0530

    Heroku testing

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+app.get("/", function (req, res) {
+    res.send("<h1>Hello World!</h1>")
+})
+
 app.listen(port, () => {
-    console.log('Server started..')
+    console.log('Server started at ', port, '..')
 })
